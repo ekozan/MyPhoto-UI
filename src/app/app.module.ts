ceb5adc403dc7b5cac9466fafd4c22e10060ce66
ekozan <geeked7@gmail.com>
2019-07-24 10:26:20 +0200

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+import { HttpClientModule }    from '@angular/common/http';
+
+
+import { AppRouterModule }     from './app-router.module';
+
+
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, FormsModule,AppRouterModule ],
+  declarations: [ AppComponent, HelloComponent, DashboardComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
